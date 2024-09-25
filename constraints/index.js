4bc8937c4f186d0be1d26a7bb4bd8e5e93e3947c
Merit <merit@flatfile.io>
2024-09-25 12:34:37 -0700

    WIP
    Signed-off-by: Merit <merit@flatfile.io>

--- constraints/index.js
+++ constraints/index.js
@@ -14,6 +14,7 @@ import {
 
 async function getValidators(event) {
   const constraints = await getConstraints(event.context.appId)
+  console.log("CONSTRAINTS API", constraints)
   return constraints.data.map((c) => {
     return {
       name: c.description,
@@ -26,15 +27,20 @@ export default function (listener) {
   listener.use(
     bulkRecordHook('**', async (records, event) => {
       if (spaceIsAutoBuild(await getSpace(event))) {
-        const externalConstraintFields = getFields(
+        const fields = getFields(
           await getSheet(event),
-        ).filter(hasExternalConstraints)
+        )
+        const externalConstraintFields = fields.filter(hasExternalConstraints)
+
         const validators = await getValidators(event)
         crossEach([records, externalConstraintFields], (record, field) => {
           getExternalConstraints(field.constraints).forEach(
             async ({ validator }) => {
               const constraint = await getValidator(validators, validator)
               if (constraint) {
+                console.log('constraint', constraint)
+                console.log('record', record)
+                console.log('field', field)
                 applyConstraintToRecord(constraint, record, field)
               }
             },
--- constraints/utils.ts
+++ constraints/utils.ts
@@ -7,8 +7,8 @@ export const getFields = (s) => s.data.config.fields
 export const spaceIsAutoBuild = (s) => s.data.metadata.isAutoBuildWorkingSpace
 export const getExternalConstraints = (c) =>
   c?.filter((c) => c.type == 'external')
-export const hasExternalConstraints = (f) =>
-  getExternalConstraints(f.constraints).length > 0
+export const hasExternalConstraints = (f) => 
+  getExternalConstraints(f.constraints || []).length > 0
 export const getValidator = (v, n) => v.find((w) => w.name === n)
 export const applyConstraintToRecord = (c, r, f) => {
   try {
@@ -20,23 +20,4 @@ export const applyConstraintToRecord = (c, r, f) => {
 export const crossProduct = (...a) =>
   a.reduce((u, c) => _.flatMap(u, (a) => c.map((b) => a.concat(b))), [[]])
 export const crossEach = (a, cb) => crossProduct(...a).forEach((p) => cb(...p))
-
-// TEMP
-import axios from 'axios'
-const axiosAPI = axios.create({})
-export const getConstraints = async (appId) => {
-  try {
-    const response = await axiosAPI.get(`/v1/apps/${appId}/constraints`, {
-      headers: {
-        Authorization: `Bearer ${process.env.FLATFILE_API_KEY}`,
-      },
-    })
-    return response.data
-  } catch (error) {
-    console.error('Error fetching app constraints:', error)
-    throw error
-  }
-}
-
-// Replace with this when constraints is no longer marked internal
-// export const getConstraints = (e) => api.constraints.get(e.context.appId)
+export const getConstraints = (e) => api.constraints.list(e.context.appId)
